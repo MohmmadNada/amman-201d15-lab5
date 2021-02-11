f17ea27c58b68432ad91a4513ba937d60debbe13
MohmmadNada <mohammad.nada_97@yahoo.com>
2021-02-11 14:55:36 +0200

    test problem5 pass git push origin problem5 git status

--- app.js
+++ app.js
@@ -120,11 +120,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
+  let multiplyP5 = multiply(testArray[0],testArray[1]);
+  multiplyP5 = multiply(multiplyP5[0],testArray[2]);
+  multiplyP5 = multiplyP5[0];
+console.log(multiplyP5);
+return [multiplyP5,`The numbers 2,3,4 have a product of ${multiplyP5}.`]
   //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
